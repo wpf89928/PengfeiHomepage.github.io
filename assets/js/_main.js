09a90efa7b0fa729289f7c4d1d73fd2e4d2ae09f
wpf89928 <64486601+wpf89928@users.noreply.github.com>
2020-12-04 12:40:01 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,10 +1,12 @@
 /* ==========================================================================
    jQuery plugin settings and other scripts
    ========================================================================== */
-<script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
-<span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
 
 $(document).ready(function(){
+   
+  <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+  <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
+   
    // Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
